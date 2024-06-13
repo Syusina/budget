c3fdf9400c26958fe89edacd24229f5246b54846
Syusina Daria <syusinad@mail.ru>
2024-06-13 11:36:20 +0500

    add folders

--- scripts.js
+++ scripts.js
@@ -32,87 +32,47 @@ const bodyHeadTitleArray = [
   "План",
 ];
 
-const dataInit = [
+const dataInitIncome = [
   {
     name: "Бюджет доходов",
     type: "FOLDER",
-    price: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    price: [4000000, 4000000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     child: [
       {
         name: "Разработка проекта",
         type: "FILE",
-        price: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        price: [2000000, 2000000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       },
       {
         name: "Ремонтные работы",
         type: "FILE",
-        price: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        price: [1500000, 1500000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       },
       {
         name: "Услуги",
         type: "FOLDER",
-        price: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        price: [500000, 500000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         child: [
           {
             name: "Аудит и консалтинг",
             type: "FILE",
-            price: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            price: [250000, 250000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
           },
           {
             name: "Работа электрика",
             type: "FILE",
-            price: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            price: [200000, 200000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          },
+
+          {
+            name: "Малярные работы",
+            type: "FILE",
+            price: [50000, 50000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
           },
           {
-            name: "Ремонт",
-            type: "FOLDER",
-            price: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            child: [
-              {
-                name: "Малярные работы",
-                type: "FILE",
-                price: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              },
-              {
-                name: "Доставка",
-                type: "FILE",
-                price: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              },
-            ],
-          },
-        ],
-      },
-      {
-        name: "Арбитраж",
-        type: "FOLDER",
-        price: [10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        child: [
-          {
-            name: "Аудит и консалтинг",
+            name: "Доставка",
             type: "FILE",
             price: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          },
-          {
-            name: "Работа электрика",
-            type: "FILE",
-            price: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          },
-          {
-            name: "Ремонт1",
-            type: "FOLDER",
-            price: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            child: [
-              {
-                name: "Малярные работы",
-                type: "FILE",
-                price: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              },
-              {
-                name: "Доставка",
-                type: "FILE",
-                price: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              },
-            ],
           },
         ],
       },
@@ -120,40 +80,125 @@ const dataInit = [
   },
 ];
 
+const dataInitExpenses = [
+  {
+    name: "Бюджет расходов",
+    type: "FOLDER",
+    price: [4000000, 4000000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    child: [
+      {
+        name: "Разработка проекта",
+        type: "FILE",
+        price: [2000000, 2000000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      },
+      {
+        name: "Ремонтные работы",
+        type: "FILE",
+        price: [1500000, 1500000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      },
+      {
+        name: "Услуги",
+        type: "FOLDER",
+        price: [500000, 500000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        child: [
+          {
+            name: "Аудит и консалтинг",
+            type: "FILE",
+            price: [250000, 250000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          },
+          {
+            name: "Работа электрика",
+            type: "FILE",
+            price: [200000, 200000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          },
+
+          {
+            name: "Малярные работы",
+            type: "FILE",
+            price: [50000, 50000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          },
+          {
+            name: "Доставка",
+            type: "FILE",
+            price: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          },
+        ],
+      },
+    ],
+  },
+];
+
+function convertToNumber(value) {
+  return Number(value.replace(",", ".").replace(/[^\d.-]/g, ""));
+}
+
+function convertToCurrency(value) {
+  return Intl.NumberFormat("ru", {
+    style: "currency",
+    currency: "RUB",
+  }).format(value);
+}
+
 function sort() {
   const element = document.querySelector(".iconSort");
   const tooltip = element.closest("div").querySelector(".tooltip");
-  const folderNames = Array.from(document.querySelectorAll("#title_l1"));
+
   if (element.src.endsWith("sort-descending.png")) {
     element.src = "./Icons/sort-alphabet.png";
     tooltip.textContent = "Сортировка статей по алфавиту";
+    const folderNames = Array.from(document.querySelectorAll("#title_l1"));
+    const sortedFolderNames = folderNames.sort((a, b) => {
+      const nameA = a.textContent.toLowerCase();
+      const nameB = b.textContent.toLowerCase();
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    });
+
+    const tbody = document.querySelector(`[id^="body_0"]`);
+    sortedFolderNames.forEach((folder) => {
+      if (folder.closest("tr").className !== "value") {
+        const element = folder.closest("tbody");
+        tbody.appendChild(element);
+      } else {
+        const parentTr = folder.closest("tr");
+        tbody.appendChild(parentTr);
+      }
+    });
   } else {
     element.src = "./Icons/sort-descending.png";
     tooltip.textContent = "Сортировка статей по убыванию итоговой суммы";
+    const folderValues = Array.from(
+      document.querySelectorAll(`[id^="sum_m0_l1`)
+    );
+    const fileValues = Array.from(document.querySelectorAll(`[id^="el_m0_l1`));
+    const allValues = [...fileValues, ...folderValues];
+    const sortedFolderNames = allValues.sort((a, b) => {
+      const nameA = a.textContent.toLowerCase();
+      const nameB = b.textContent.toLowerCase();
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    });
+    const tbody = document.querySelector(`[id^="body_0"]`);
+    sortedFolderNames.forEach((folder) => {
+      if (folder.closest("tr").className !== "value") {
+        const element = folder.closest("tbody");
+        tbody.appendChild(element);
+      } else {
+        const parentTr = folder.closest("tr");
+        tbody.appendChild(parentTr);
+      }
+    });
   }
-
-  const sortedFolderNames = folderNames.sort((a, b) => {
-    const nameA = a.textContent.toLowerCase();
-    const nameB = b.textContent.toLowerCase();
-    if (nameA < nameB) {
-      return -1;
-    }
-    if (nameA > nameB) {
-      return 1;
-    }
-    return 0;
-  });
-
-  const tbody = document.querySelector(`[id^="body_0"]`);
-  sortedFolderNames.forEach((folder) => {
-    if (folder.closest("tr").className !== "value") {
-      const element = folder.closest("tbody");
-      tbody.appendChild(element);
-    } else {
-      const parentTr = folder.closest("tr");
-      tbody.appendChild(parentTr);
-    }
-  });
 }
 
 function hide(event) {
@@ -166,10 +211,48 @@ function hide(event) {
   });
 }
 
-function deleteRow(event) {
+function deleteRow(event, level) {
   const element = event.target.closest("tr");
   const parent = element.closest("tbody");
   parent.removeChild(element);
+  const months = [0, 1, 2, 3, 4, 5, 6, 7, 8, 8, 10, 11, 12];
+
+  months.forEach((month) => {
+    const valuesColumnFile = parent.querySelectorAll(
+      `[id^="el_m${month}_l${level}"]`
+    );
+    const valuesColumnFolder = parent.querySelectorAll(
+      `[id^="sum_m${month}_l${level}"]`
+    );
+    for (let i = level; i > 0; i -= 1) {
+      const sumColumn = document.querySelector(
+        `[id^="sum_m${month}_l${i - 1}"]`
+      );
+      let sumColumnValue = 0;
+      valuesColumnFile.forEach((value) => {
+        sumColumnValue += convertToNumber(value.textContent);
+      });
+
+      if (valuesColumnFolder) {
+        valuesColumnFolder.forEach((value) => {
+          sumColumnValue += convertToNumber(value.textContent);
+        });
+        sumColumn.textContent = convertToCurrency(sumColumnValue);
+      }
+
+      const sumRowAll = document.querySelector(`[id^="sum_m0_l${i - 1}"]`);
+      const resultRow = sumRowAll.closest("tr");
+      const resultValues = resultRow.querySelectorAll(`[id^="sum_m"]`);
+
+      let sumRowValue = 0;
+      resultValues.forEach((value, index) => {
+        if (index !== 0) {
+          sumRowValue += convertToNumber(value.textContent);
+        }
+      });
+      sumRowAll.textContent = convertToCurrency(sumRowValue);
+    }
+  });
 }
 
 function addFile(event, level) {
@@ -192,6 +275,7 @@ function addFile(event, level) {
       const nameRow = event.target.value;
       const newName = document.createElement("th");
       newName.classList.add("bodyNameCell");
+      newName.id = `title_l${level + 1}`;
       newName.style.paddingLeft = (level + 1) * 32 + "px";
       newName.textContent = nameRow;
       const imgCancel = document.createElement("img");
@@ -203,17 +287,14 @@ function addFile(event, level) {
       imgCancel.addEventListener("mouseout", function () {
         imgCancel.classList.remove("iconCancelActive");
       });
-      imgCancel.setAttribute("onclick", "deleteRow(event)");
+      imgCancel.setAttribute("onclick", `deleteRow(event, ${level + 1})`);
       newName.appendChild(imgCancel);
 
       newCell.appendChild(newName);
 
       for (let i = 0; i < 13; i += 1) {
         const valueCell = document.createElement("td");
-        valueCell.textContent = Intl.NumberFormat("ru", {
-          style: "currency",
-          currency: "RUB",
-        }).format(0);
+        valueCell.textContent = convertToCurrency(0);
         valueCell.classList.add("bodyValueCell");
         if (i > 0) {
           valueCell.style.cursor = "pointer";
@@ -225,6 +306,119 @@ function addFile(event, level) {
 
         valueCell.id = `el_m${i}_l${level + 1}`;
         newCell.appendChild(valueCell);
+      }
+      if (event.target.value !== "") {
+        parent.appendChild(newCell);
+      }
+      element.innerHTML = initText;
+    }
+  });
+}
+
+function addFolder(event, level) {
+  event.target.style.background = "none";
+  const element = event.target.closest("th");
+  const parent = event.target.closest("tbody");
+
+  const tr = document.createElement("tr");
+  tr.classList.add(`nameFolder_${level}`);
+  const initText = element.innerHTML;
+  element.innerHTML = "";
+  const input = document.createElement("input");
+  input.classList.add("input");
+  input.placeholder = "Название группы...";
+  element.appendChild(input);
+  input.focus();
+
+  input.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      let isHide = false;
+      const newCell = document.createElement("tbody");
+      const nameRow = event.target.value;
+      newCell.id = `body_${level}_${nameRow}`;
+      const newName = document.createElement("th");
+      newName.classList.add("bodyNameCell");
+      newName.id = `title_l${level + 1}`;
+
+      const nameContainer = document.createElement("div");
+      nameContainer.classList.add("nameContainer");
+      nameContainer.id = "btn-hide";
+      nameContainer.setAttribute("onclick", "hide(event)");
+      nameContainer.style.paddingLeft = level * 32 + "px";
+
+      const label = document.createElement("label");
+      label.style.cursor = "pointer";
+      label.textContent = nameRow;
+
+      nameContainer.appendChild(label);
+      const iconsContainer = document.createElement("div");
+      iconsContainer.classList.add("iconsContainer");
+      const imgAddFile = document.createElement("img");
+      imgAddFile.src = "./Icons/plus.png";
+      imgAddFile.classList.add("iconAdd");
+
+      imgAddFile.addEventListener("mouseover", function () {
+        imgAddFile.style.background = "rgba(0, 0, 0, 0.1)";
+      });
+
+      imgAddFile.addEventListener("mouseout", function () {
+        imgAddFile.style.background = "none";
+      });
+
+      imgAddFile.setAttribute("onclick", `addFile(event, ${level + 1})`);
+
+      const imgAddFolder = document.createElement("img");
+      imgAddFolder.src = "./Icons/folder.png";
+      imgAddFolder.classList.add("iconAdd");
+
+      imgAddFolder.addEventListener("mouseover", function () {
+        imgAddFolder.style.background = "rgba(0, 0, 0, 0.1)";
+      });
+
+      imgAddFolder.addEventListener("mouseout", function () {
+        imgAddFolder.style.background = "none";
+      });
+      imgAddFolder.setAttribute("onclick", `addFolder(event, ${level + 1})`);
+
+      iconsContainer.appendChild(imgAddFolder);
+      iconsContainer.appendChild(imgAddFile);
+
+      newName.appendChild(nameContainer);
+      newName.appendChild(iconsContainer);
+
+      if (level > 0) {
+        const imgHide = document.createElement("img");
+        imgHide.src = "./Icons/mini-arrow.png";
+        imgHide.id = "icon-hide";
+        imgHide.classList.add("iconHide");
+
+        nameContainer.addEventListener("click", () => {
+          if (!isHide) {
+            imgHide.style.transition = "transform 0.3s ease-in-out";
+            imgHide.style.transform = "rotate(90deg)";
+            isHide = true;
+          } else {
+            imgHide.style.transition = "transform 0.3s ease-in-out";
+            imgHide.style.transform = "rotate(0deg)";
+            isHide = false;
+          }
+        });
+
+        nameContainer.insertBefore(imgHide, nameContainer.firstChild);
+        newName.style.paddingLeft = level * 32 + "px";
+      }
+
+      tr.appendChild(newName);
+
+      newCell.appendChild(tr);
+      for (let i = 0; i < 13; i += 1) {
+        const valueCell = document.createElement("td");
+        valueCell.textContent = convertToCurrency(0);
+        valueCell.classList.add("bodyValueCell");
+
+        valueCell.id = `sum_m${i}_l${level + 1}`;
+        tr.appendChild(valueCell);
       }
       if (event.target.value !== "") {
         parent.appendChild(newCell);
@@ -252,6 +446,7 @@ function change(event, month, level) {
   element.textContent = "";
 
   const input = document.createElement("input");
+  input.setAttribute("type", "number");
   input.id = `el_m${month}_l${level - 1}`;
   input.classList.add("inputValue");
   input.placeholder = "";
@@ -261,23 +456,22 @@ function change(event, month, level) {
   input.addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
       event.preventDefault();
-      const newValue = event.target.value;
-      element.textContent = Intl.NumberFormat("ru", {
-        style: "currency",
-        currency: "RUB",
-      }).format(newValue);
+      const newValue = convertToCurrency(event.target.value);
+      element.textContent = newValue;
       changeName = newValue;
 
       let sumRowValue = 0;
       valuesRow.forEach((value, index) => {
         if (index !== 0) {
-          sumRowValue += parseInt(value.textContent);
+          sumRowValue += Number(
+            value.textContent
+              .replace(",", ".")
+              .replace(",", ".")
+              .replace(/[^\d.-]/g, "")
+          );
         }
       });
-      sumRow.textContent = Intl.NumberFormat("ru", {
-        style: "currency",
-        currency: "RUB",
-      }).format(sumRowValue);
+      sumRow.textContent = convertToCurrency(sumRowValue);
 
       for (let i = level; i > 0; i -= 1) {
         const sumColumn = document.querySelector(
@@ -285,17 +479,14 @@ function change(event, month, level) {
         );
         let sumColumnValue = 0;
         valuesColumnFile.forEach((value) => {
-          sumColumnValue += parseInt(value.textContent);
+          sumColumnValue += convertToNumber(value.textContent);
         });
 
         if (valuesColumnFolder) {
           valuesColumnFolder.forEach((value) => {
-            sumColumnValue += parseInt(value.textContent);
+            sumColumnValue += convertToNumber(value.textContent);
           });
-          sumColumn.textContent = Intl.NumberFormat("ru", {
-            style: "currency",
-            currency: "RUB",
-          }).format(sumColumnValue);
+          sumColumn.textContent = convertToCurrency(sumColumnValue);
         }
 
         const sumRowAll = document.querySelector(`[id^="sum_m0_l${i - 1}"]`);
@@ -305,17 +496,15 @@ function change(event, month, level) {
         let sumRowValue = 0;
         resultValues.forEach((value, index) => {
           if (index !== 0) {
-            sumRowValue += parseInt(value.textContent);
+            sumRowValue += convertToNumber(value.textContent);
           }
         });
-        sumRowAll.textContent = Intl.NumberFormat("ru", {
-          style: "currency",
-          currency: "RUB",
-        }).format(sumRowValue);
+        sumRowAll.textContent = convertToCurrency(sumRowValue);
       }
     }
+
     input.addEventListener("blur", function () {
-      if (changeName !== event.target.value) {
+      if (convertToNumber(changeName) !== event.target.value) {
         element.textContent = initText;
       }
     });
@@ -428,6 +617,7 @@ function renderData(data, folderName, body, level) {
       imgAddFolder.addEventListener("mouseout", function () {
         imgAddFolder.style.background = "none";
       });
+      imgAddFolder.setAttribute("onclick", `addFolder(event, ${level})`);
 
       iconsContainer.appendChild(imgAddFolder);
       iconsContainer.appendChild(imgAddFile);
@@ -462,10 +652,7 @@ function renderData(data, folderName, body, level) {
       element.price.forEach((el, index) => {
         const td = document.createElement("td");
         td.id = `sum_m${index}_l${level}`;
-        td.textContent = Intl.NumberFormat("ru", {
-          style: "currency",
-          currency: "RUB",
-        }).format(0);
+        td.textContent = convertToCurrency(el);
         if (level === 0) {
           td.classList.add("bodyTitleCell");
         } else {
@@ -496,16 +683,13 @@ function renderData(data, folderName, body, level) {
       imgCancel.addEventListener("mouseout", function () {
         imgCancel.classList.remove("iconCancelActive");
       });
-      imgCancel.setAttribute("onclick", "deleteRow(event)");
+      imgCancel.setAttribute("onclick", `deleteRow(event, ${level})`);
       th.appendChild(imgCancel);
       tr.appendChild(th);
 
       element.price.forEach((el, index) => {
         const td = document.createElement("td");
-        td.textContent = Intl.NumberFormat("ru", {
-          style: "currency",
-          currency: "RUB",
-        }).format(0);
+        td.textContent = convertToCurrency(el);
         td.classList.add("bodyValueCell");
         if (index > 0) {
           td.setAttribute("onclick", `change(event, ${index}, ${level})`);
@@ -534,4 +718,4 @@ table.classList.add("table");
 const tableContainer = document.getElementById("tableContainer");
 tableContainer.appendChild(table);
 
-renderData(dataInit, "", tbodyData, 0);
+renderData(dataInitIncome, "", tbodyData, 0);
